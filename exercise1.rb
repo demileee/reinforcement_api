ff9fcd2718ae7872928dae453e9c39156ff8df88
Demi Lee <demilee.to@gmail.com>
2017-11-12 23:18:29 -0500

    exercise

---
+++ exercise1.rb
@@ -0,0 +1,6 @@
+require 'httparty'
+
+toronto_wards_response = HTTParty.get('https://represent.opennorth.ca/boundaries/toronto-wards/')
+toronto_wards_json = JSON.parse(toronto_wards_response.body)
+
+name = toronto_wards_json["objects"].map {|ward| ward["name"]}
